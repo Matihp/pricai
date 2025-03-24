17971b8fde8fb01e459c3370ba6ab16764dfff6d
Matias Contreras <matiasleoncontreras@gmail.com>
2025-03-23 22:42:16 -0300

    update package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing: {
+      prefixDefaultLocale: true
+    }
+  },
+  output: 'server',
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss(),TanStackRouterVite()]
   }
 });
