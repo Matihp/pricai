4cfb295caf386b6bbb48b8ba08558d7f03e248c5
Matias Contreras <matiasleoncontreras@gmail.com>
2025-04-08 18:51:16 -0300

    refactor id-page

---
+++ src/components/pricing/ServiceDetailContainer.tsx
@@ -0,0 +1,20 @@
+import { type ReactNode } from "react";
+import { CompareProvider } from "../../contexts/CompareContext";
+import CompareFloatingButton from "./CompareFloatingButton";
+import type { SupportedLocale } from "../../utils/i18n";
+
+interface ServiceDetailContainerProps {
+  children: ReactNode;
+  locale: SupportedLocale;
+}
+
+export default function ServiceDetailContainer({ children, locale }: ServiceDetailContainerProps) {
+  return (
+    <CompareProvider>
+      <div className="relative">
+        {children}
+        <CompareFloatingButton locale={locale} />
+      </div>
+    </CompareProvider>
+  );
+}
