8d3c6e1b25732aa3d66765a89af3efd1678588d9
matias1364 <matiasleoncontreras@gmail.com>
2025-04-22 20:57:11 -0300

    change ai-data

--- src/data/ai-data.ts
+++ src/data/ai-data.ts
@@ -1,23 +1,48 @@
 export interface AIService {
-    id: string;
-    name: string;
-    description: {
-      es: string;
-      en: string;
-    };
-    price: string;
-    priceDetails: string;
-    categories: string[];
-    type: "api" | "individual" | "code-editor";
-    features: {
-      es: string[];
-      en: string[];
-    };
-    rating: number;
-    hasFree: boolean;
-    hasAPI: boolean;
-    commercialUse: boolean;
-    customModels: boolean;
-    isNew: boolean;
-    releaseYear: number;
-  }
+  id: string;
+  name: string;
+  description: Description;
+  models: AIModel[];
+  priceDetails: string;
+  categories: string[];
+  types: ("api" | "individual" | "code-editor")[]; 
+  features: {
+    es: string[];
+    en: string[];
+  };
+  tools?: {
+    [toolName: string]: string | { 
+      [modelName: string]: {
+        [level: string]: string;
+      }
+    }
+  };
+  hasFree: boolean;
+  hasAPI: boolean;
+  commercialUse: boolean;
+  customModels: boolean;
+  isNew: boolean;
+  releaseYear: number;
+  security?: Description;
+  support?: Description;
+  useCases?: {
+    es: string[];
+    en: string[];
+  };
+  integrations?: Description;
+}
+
+export interface AIModel {
+  name: string;
+  description: Description;
+  price: {
+    input?: string;
+    cached_input?: string;
+    output?: string;
+    [otherPriceType: string]: string | undefined;
+  };
+  context_length?: string;
+  rating: number;
+}
+
+export type Description = string | { es?: string; en?: string };
